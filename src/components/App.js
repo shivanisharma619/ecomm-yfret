2799c913e9a2e08f82fbd7e782065f3a1ea5a578
Shivani Sharma <s.shiv619@gmail.com>
2020-08-13 21:17:53 +0530

    clean code

--- src/components/App.js
+++ src/components/App.js
@@ -7,23 +7,15 @@ import {
 } from "react-router-dom";
 import { Header } from './Header';
 import { Products } from './Products';
-import { Menus } from './Menus';
-import { AntMenu } from './AntMenu';
 
 function App() {
   return (
   	<Router>
-	    <div className="mb-3">
+	     <div className="mb-3">
 	      <Switch>
 	        <Route exact path="/">
 	          <Header />
 	          <Products />
-	        </Route>
-	        <Route exact path="/menus">
-            <Menus />
-	        </Route>
-	        <Route exact path="/menu-2">
-            <AntMenu />
 	        </Route>
 	      </Switch>
 	    </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,35 +2,6 @@ import React, { useEffect } from 'react';
 
 export function Header() {
 
-  const string = 'aACeEjc';
-  console.log(string.split(''));
-
-  useEffect(() => {
-    const sol = solution(string);
-    console.log('sol', sol);
-  },[]);
-
-  const solution = (string) => {
-    const stringToArray = string.split('');
-    const sortedArray = stringToArray.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
-    console.log('sortedArray', sortedArray);
-    const array = [];
-    let i = 0;
-    for (;i < sortedArray.length; i++) {
-    	console.log('before i', i);
-    	console.log(sortedArray[i]);
-    	console.log(sortedArray[i + 1]);
-      if ((i < sortedArray.length - 1) && sortedArray[i].toLowerCase() == sortedArray[i + 1].toLowerCase()) { //0==1
-      	array.push(sortedArray[i]);
-        i = i + 1;
-      }
-    }
-
-    console.log('array', array);
-
-
-  }
-
   return (
 		<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 		  <a className="navbar-brand" id="heading" href="#">Yfret Assignment</a>
